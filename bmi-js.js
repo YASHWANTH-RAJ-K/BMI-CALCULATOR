82e23cdd92e10ad0b45d8e8f5f3a225086d28d3c
YASHWANTH-RAJ-K <kr8196@srmist.edu.in>
2021-06-13 18:23:28 +0530

    Add files via upload

--- bmi-js.js
+++ bmi-js.js
@@ -21,11 +21,11 @@ function calculateBMI(){
 
 
          if(bmi < 18.6){
-             result.innerHTML = `Under Weight : <span>${bmi}</span>`;
-         }else if(bmi >= 18.6 && bmi < 24.9){
+             result.innerHTML = `Under Weight-Choose weight gain: <span>${bmi}</span>`;
+         }else if(bmi >= 18.6 && bmi < 25){
             result.innerHTML = `Normal : <span>${bmi}</span>`;
          } else{
-            result.innerHTML = `Over Weight : <span>${bmi}</span>`;
+            result.innerHTML = `Over Weight-Choose weight loss : <span>${bmi}</span>`;
          }
     }
 }
